2c2f4855beb4276229c687f5a256d310d0130a28
ibra <ibrahim07@gmail.com>
2025-03-07 00:46:35 +0000

    update datanames

--- src/concerts/DataConcert.js
+++ src/concerts/DataConcert.js
@@ -1,30 +1,25 @@
 const concerts = [
+    {
+        img: "notus.jpg",
+        titre: "Concert de kendrick Lamar",
+        date: "2025-03-08",
+        lieu: "Théâtre Mohammed VI - Casablanca",
+        heure: "21:30",
+        prix: "600"
+    }
+    ,
   {
-      img: "concert1.jpg",
-      titre: "Festival Aji T'hdm",
-      date: "2025-03-12",
-      lieu: "Théâtre National Mohammed V - Rabat",
-      heure: "21:00",
-      prix: "200"
-  },
+    img: "concert11.jpg",
+    titre: "Concert de El Grande Toto",
+    date: "2025-03-25",
+    lieu: "Arena Hall Rabat",
+    heure: "21:00",
+    prix: "200"
+},
+  
+  
   {
-      img: "concert2.jpg",
-      titre: "Hihbra Tour - Rachid Rafik",
-      date: "2025-03-09",
-      lieu: "Megarama Casablanca",
-      heure: "22:00",
-      prix: "180"
-  },
-  {
-      img: "concert3.jpg",
-      titre: "Soirée Patrimoine",
-      date: "2025-03-07",
-      lieu: "Théâtre Mohammed VI - Casablanca",
-      heure: "21:30",
-      prix: "250"
-  },
-  {
-      img: "concert4.jpg",
+      img: "concert8.jpg",
       titre: "Maestro Arslane",
       date: "2025-04-05",
       lieu: "Megarama Casablanca",
@@ -32,69 +27,77 @@ const concerts = [
       prix: "220"
   },
   {
-      img: "concert5.jpg",
-      titre: "Les Inqualifiables - Entre Amis",
-      date: "2025-03-11",
-      lieu: "Meydene Marrakech",
-      heure: "21:00",
-      prix: "170"
-  },
-  {
-      img: "concert6.jpg",
-      titre: "Said & Wadie - Ztektaw",
-      date: "2025-03-21",
-      lieu: "Théâtre National Mohammed V - Rabat",
-      heure: "22:00",
-      prix: "190"
-  },
-  {
-      img: "concert7.jpg",
-      titre: "Bassou - Man Zakin",
-      date: "2025-03-06",
-      lieu: "Megarama Casablanca",
-      heure: "22:00",
-      prix: "160"
-  },
-  {
-      img: "concert8.jpg",
-      titre: "Yassar - Lamhayab",
-      date: "2025-03-15",
-      lieu: "Complexe Mohammed V - Casablanca",
-      heure: "22:00",
-      prix: "210"
-  },
+    img: "concert10.jpg",
+    titre: "Concert de Beyoncé",
+    date: "2025-03-30",
+    lieu: "Stadium de Houston",
+    heure: "20:00",
+    prix: "700"
+},
+{
+    img: "concert4.jpg",
+    titre: "Yassar - Lamhayab",
+    date: "2025-03-15",
+    lieu: "Complexe Mohammed V - Casablanca",
+    heure: "22:00",
+    prix: "210"
+},
+ 
+
+
+
+{
+    img: "concert12.jpg",
+    titre: "Lana Del Rey",
+    date: "2025-03-22",
+    lieu: "Tomorrowland",
+    heure: "19:00",
+    prix: "600"
+},
+{
+    img: "concert3.jpg",
+    titre: "Les Inqualifiables",
+    date: "2025-03-07",
+    lieu: "Théâtre Mohammed VI - Casablanca",
+    heure: "21:30",
+    prix: "250"
+},
+{
+    img: "weekend.jpg",
+    titre: "The Weekend",
+    date: "2025-03-22",
+    lieu: "Tomorrowland",
+    heure: "19:00",
+    prix: "600"
+},
+
+  
+ 
   {
       img: "concert9.jpg",
-      titre: "Concert Hip-Hop Night",
+      titre: "Rachid Rafik",
       date: "2025-03-28",
       lieu: "Grand Théâtre de Casablanca",
       heure: "20:30",
       prix: "150"
-  },
-  {
-      img: "concert10.jpg",
-      titre: "Festival Gnawa Fusion",
-      date: "2025-03-30",
-      lieu: "Plage de Bouznika",
-      heure: "18:00",
-      prix: "130"
-  },
-  {
-      img: "concert11.jpg",
-      titre: "Comedy Stars Night",
-      date: "2025-03-25",
-      lieu: "Salle Bahnini Rabat",
-      heure: "21:00",
-      prix: "200"
-  },
-  {
-      img: "concert12.jpg",
-      titre: "Rock Legends Tribute",
-      date: "2025-03-22",
-      lieu: "Casablanca Arena",
-      heure: "19:00",
-      prix: "180"
-  }
+  },{
+    img: "concert1.jpg",
+    titre: "Festival Aji T'hdm",
+    date: "2025-03-12",
+    lieu: "Théâtre National Mohammed V - Rabat",
+    heure: "21:00",
+    prix: "200"
+},{
+    img: "concert7.jpg",
+    titre: "Bassou - Man Zakin",
+    date: "2025-03-06",
+    lieu: "Megarama Casablanca",
+    heure: "22:00",
+    prix: "160"
+},
+  
+  
+  
 ];
 
 export default concerts;
